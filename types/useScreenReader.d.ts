90b2a2e50b4c90b8e300cca74d182ad1ee47b193
Denys Kashkovskyi <denys.kashkovskyi@evry.com>
2019-06-08 15:20:04 +0300

    Remove unused types

--- types/useScreenReader.d.ts
+++ types/useScreenReader.d.ts
@@ -1,26 +1,12 @@
 /// <reference types="react" />
-declare type ReaderState = {
-    [msg: string]: {
-        focused: boolean;
-        handlers: {
-            onFocus: () => void;
-            onBlur: () => void;
-        };
-    };
-};
-export declare const actionTypes: {
-    FOCUS: string;
-    BLUR: string;
-    SET: string;
-};
-export declare const elementReducer: (state: ReaderState, action: any) => ReaderState;
-export declare const AriaLiveMessage: (props: any) => JSX.Element;
 declare const useScreenReader: () => {
-    a11y: () => JSX.Element | null;
-    reader: (message: string) => {
-        onFocus: () => void;
-        onBlur: () => void;
-    };
-    read: (message: string) => void;
+	a11y: () => JSX.Element | null;
+	reader: (
+		message: string
+	) => {
+		onFocus: () => void;
+		onBlur: () => void;
+	};
+	read: (message: string) => void;
 };
 export default useScreenReader;
